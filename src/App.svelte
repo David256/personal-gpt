3b5ee055dee6fb25e0dcee8bbbbec4217bec6fca
David256 <wasterdavid@gmail.com>
2024-05-02 18:26:27 -0500

    Update the Front-End: implement BackEnd commands

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,42 @@
 <script lang="ts">
+  import { invoke } from "@tauri-apps/api/tauri";
+
   import Microphone from "./lib/Microphone.svelte";
+  import DeviceList from "./lib/DeviceList.svelte";
+
+  let deviceNames: string[] = [];
+  let deviceIndex = 0;
+
+  let isRecording = false;
+
+  const onStartRecording = async () => {
+    if (isRecording) return;
+    const result = await invoke("start_recording", { index: deviceIndex });
+    console.log("start recording:", result);
+    isRecording = true;
+  };
+
+  const onStopRecording = async () => {
+    if (!isRecording) return;
+    const result = await invoke("stop_recording");
+    console.log("stop recording:", result);
+    isRecording = false;
+  };
+
+  const requestDeviceNames = async () => {
+    const result = await invoke("list_devices");
+    console.log("devices:", result);
+    return result as string[];
+  };
+
+  const updateDeviceIndex = (index: number) => {
+    deviceIndex = index;
+  };
+
+  requestDeviceNames().then((names) => {
+    console.log("devices:", names);
+    deviceNames = names;
+  });
 </script>
 
 <main class="container">
@@ -9,9 +46,14 @@
     </div>
   </div>
 
-  <div class="row">
-    <div class="col p-2">
-      <Microphone />
+  <div class="row h-full">
+    <div class="col p-2 space-between">
+      <Microphone {onStartRecording} {onStopRecording} />
+      <DeviceList
+        {deviceIndex}
+        devices={deviceNames}
+        onSelect={updateDeviceIndex}
+      />
     </div>
   </div>
 </main>
