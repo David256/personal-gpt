a1b9b5c1c8efe922ec481c7ea373aa30fced4549
David256 <wasterdavid@gmail.com>
2024-05-13 09:02:02 -0500

    Add support to TTS and VTT

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,17 +4,24 @@
 use std::error::Error;
 use std::path::PathBuf;
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Mutex;
 use std::{env, thread};
 
 use async_openai::types::{
     AudioResponseFormat, ChatCompletionRequestMessage, ChatCompletionRequestSystemMessageArgs,
-    ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequestArgs,
-    CreateTranscriptionRequestArgs,
+    ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequestArgs, CreateSpeechRequestArgs,
+    CreateTranscriptionRequestArgs, SpeechModel, Voice,
 };
 use async_openai::Client;
 use chrono::Local;
 
 use pv_recorder::PvRecorderBuilder;
+use serde::{Deserialize, Serialize};
+
+// Audio
+use rodio::{source::Source, Decoder, OutputStream};
+use std::fs::File;
+use std::io::BufReader;
 
 static mut FILENAME: String = String::new();
 static mut SAMPLE_RATE: usize = 16000;
@@ -23,10 +30,23 @@ static LISTENING: AtomicBool = AtomicBool::new(false);
 static SAVED: AtomicBool = AtomicBool::new(false);
 static WAS_ERROR: AtomicBool = AtomicBool::new(false);
 
+type WrappedHistorials = Mutex<Option<Vec<Historial>>>;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct Historial {
+    role: String,
+    content: String,
+}
+
+static HISTORIALS: Vec<Historial> = Vec::new();
+static mut ABC: Vec<ChatCompletionRequestMessage> = Vec::new();
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
 async fn reply_chat(input: String) -> Result<String, Box<dyn Error>> {
     let client = Client::new();
+
+    // unsafe { ABC.append(ChatCompletionRequestUserMessageArgs::default().content(input)) }
 
     let request = CreateChatCompletionRequestArgs::default()
         .max_tokens(3000u16)
@@ -75,6 +95,47 @@ async fn transcribe_srt(filename: String) -> Result<String, Box<dyn Error>> {
     Ok(response.text)
 }
 
+async fn text_to_speech(input: String) -> Result<(), Box<dyn Error>> {
+    let client = Client::new();
+
+    let request = CreateSpeechRequestArgs::default()
+        .input(input)
+        .voice(Voice::Nova)
+        .model(SpeechModel::Tts1Hd)
+        .build()?;
+
+    let response = client.audio().speech(request).await?;
+
+    // Create the temporal file
+    let filename = Local::now().format("%Y-%m-%d %H:%M:%S.mp3").to_string();
+    let temp_dir = env::temp_dir();
+    let mut temp_path = PathBuf::new();
+    temp_path.push(temp_dir);
+    temp_path.push(filename);
+    let abs_path = temp_path.to_string_lossy().to_string();
+    println!("temp_path = {}", abs_path);
+
+    let audio_path = abs_path.clone();
+
+    // Save it
+    response.save(abs_path).await?;
+
+    // Get an output stream handle to the default physical sound device
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    // Load a sound from a file, using a path relative to Cargo.toml
+    let file = BufReader::new(File::open(audio_path).unwrap());
+    // Decode that sound file into a source
+    let source = Decoder::new(file).unwrap();
+    // Play the sound directly on the device
+    stream_handle.play_raw(source.convert_samples());
+
+    // The sound plays in a separate audio thread,
+    // so we need to keep the main thread alive while it's playing.
+    std::thread::sleep(std::time::Duration::from_secs(5));
+
+    Ok(())
+}
+
 #[tauri::command(async)]
 async fn reply_as_assistant(input: &str) -> Result<String, ()> {
     let data = match reply_chat(input.to_owned()).await {
@@ -87,6 +148,12 @@ async fn reply_as_assistant(input: &str) -> Result<String, ()> {
     };
 
     Ok(data)
+}
+
+#[tauri::command]
+async fn speech(input: &str) -> Result<(), ()> {
+    text_to_speech(input.to_owned()).await;
+    Ok(())
 }
 
 #[tauri::command(async)]
@@ -236,6 +303,7 @@ fn main() {
             speech_to_text,
             get_error_message,
             reply_as_assistant,
+            speech,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
